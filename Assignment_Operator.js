68afded9f1a8294ef49a968fadba9a692d04de68
Muhammad-Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2023-01-16 10:37:24 -0800

    Add files via upload

---
+++ Assignment_Operator.js
@@ -0,0 +1,23 @@
+num1 = 10
+
+num1 +=5
+num1 =num1 +5
+num1++
+num1 +=1
+num1 = num1 +1
+
+console.log(num1)
+
+num1 -=5
+num1 =num1 -5
+num1--
+num1 -=1
+num1 = num1 -1
+
+console.log(num1)
+
+
+num1 *=5
+num1 =num1 *5
+
+console.log(num1)
